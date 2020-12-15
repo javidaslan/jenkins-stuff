329bd8b7dfdb18f55df3c4027f30b7807cdea5bf
Javid Aslanov <javid.aslanov@prezi.com>
2020-12-15 22:09:08 +0100

    quick fix

--- pipeline/jenkins/build/maven.sh
+++ pipeline/jenkins/build/maven.sh
@@ -4,4 +4,4 @@ echo "**********************************"
 echo "********* Building jar ***********"
 echo "**********************************"
 
-docker run --rm -v $PWD/java-app:/app -v /root/.m2:/root/.m2 maven:3-alpine -w /app mvn "$@"
+docker run --rm -v $PWD/java-app:/app -v /root/.m2:/root/.m2 -w /app maven:3-alpine "$@"
